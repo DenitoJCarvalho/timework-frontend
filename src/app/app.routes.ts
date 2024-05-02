da10589bc957fd718d2932172e3c069eaffccb90
Dennis <dennisjcarvalho@gmail.com>
2024-05-02 20:51:32 -0300

    feat: Novos pacotes e configuracoes Material UI Rotas para login, worksheet e coringas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './pages/login/login.component';
+import { WorksheetComponent } from './pages/worksheet/worksheet.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+export const routes: Routes = [
+
+  { path: '', pathMatch: 'full', redirectTo: '/login' },
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'worksheet', component: WorksheetComponent, pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent, pathMatch: 'full' }
+
+];
