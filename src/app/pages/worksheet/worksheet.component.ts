9c74330bdd4b86da6f4fd079653cb4e99076a444
Dennis <dennisjcarvalho@gmail.com>
2024-05-02 20:52:34 -0300

    feat: Novas paginas Login Worksheet NotFound

---
+++ src/app/pages/worksheet/worksheet.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-worksheet',
+  standalone: true,
+  imports: [],
+  templateUrl: './worksheet.component.html',
+  styleUrl: './worksheet.component.css'
+})
+export class WorksheetComponent {
+
+}
