084b4bd0871926d22f0ecff9d1125ef4332b19f2
Dennis <dennisjcarvalho@gmail.com>
2024-05-04 14:20:50 -0300

    feat: Pagina login Componente menu Componente footer responsividade de 320px ate 1200px

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { MenuComponent } from '../../components/menu/menu.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent, FooterComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  error: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
 
 }
