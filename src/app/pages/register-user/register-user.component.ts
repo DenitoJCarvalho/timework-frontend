b94d1d3b1e67a9c79b331c0d29e6f9a0c5ec4b79
Dennis <dennisjcarvalho@gmail.com>
2024-06-13 20:20:21 -0300

    feat: Formulario adicionado formulario validacao

--- src/app/pages/register-user/register-user.component.ts
+++ src/app/pages/register-user/register-user.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { MenuComponent } from '../../components/menu/menu.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-register-user',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MenuComponent, FooterComponent
+  ],
   templateUrl: './register-user.component.html',
   styleUrl: './register-user.component.css'
 })
 export class RegisterUserComponent {
+
+  userType: string = 'user';
+
+  formRegister = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(50),
+      Validators.minLength(3)
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6)
+    ]),
+    confirmPassword: new FormControl('', [
+      Validators.required,
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+    ]),
+    enterprise: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(40)
+    ]),
+    userType: new FormControl(this.userType)
+  });
 
 }
